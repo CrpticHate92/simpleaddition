052bd6f91881fac0204c75e75494491ee22542a1
CrpticHate92 <145369850+CrpticHate92@users.noreply.github.com>
2023-12-08 09:18:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Simple Addition IPO Calculator By Mr.M
+
+// Input
+let number1 = +prompt("Enter a number:");
+let number2 = +prompt("Enter another number:");
+let number3 = +prompt("Enter your final number:");
+// Process
+let total = number1 + number2 + number3;
+// Output
+alert(`The result is: ${number1} + ${number2} + ${number3} = ${total}`);
